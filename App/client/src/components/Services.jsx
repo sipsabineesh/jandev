a5970394974d1bbe5c7032b6de4c7104e14996ae
Sipsa <sipsabineesh@gmail.com>
2026-03-23 16:15:06 +0530

    bugfix

--- App/client/src/components/Services.jsx
+++ App/client/src/components/Services.jsx
@@ -7,7 +7,7 @@ const Services = () => {
         <p className='text-2xl md:text-3xl font-medium'>Services</p>
         <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 mt-6 gap-6'>
           { categories.map((category,index) => (
-            <div key={index} className='group cursor-pointer py-5 px-3 gap-2 rounded-lg flex flex-col justify-center items-start pl-2'
+            <div key={index} className='group cursor-pointer py-5 px-3 gap-2 rounded-lg flex flex-col justify-center items-center'
             style={{backgroundColor:category.bgColor}}
             onClick={()  => {
               navigate(`/products/${category.path.toLowerCase()}`) 
