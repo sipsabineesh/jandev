5e00b82db049c46dd0bc7721926f3669b008ad11
Sipsa <sipsabineesh@gmail.com>
2026-03-23 16:12:53 +0530

    bug fix

--- App/client/src/components/BottomBanner.jsx
+++ App/client/src/components/BottomBanner.jsx
@@ -7,7 +7,7 @@ const BottomBanner = () => {
         <img src={assets.bottom_banner_image} alt="banner" className='w-full hidden md:block'/>
         <img src={assets.bottom_banner_image_sm} alt="banner" className='w-full  md:hidden'/>
           <div className='absolute inset-0 flex flex-col item-center md:items-end md:justify-center pt-16 md:pt-0 md:pr-24'>
-            <div className='ml-20'>
+            <div className=''>
                 <h1 className="">Why We Are The Best?</h1>
                 {features.map((feature,index) => (
                         <div key={index} className='flex items-center gap-4 mt-2'>
