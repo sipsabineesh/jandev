be2662850a663954ce024fe106527b15aa80aeda
Sipsa <sipsabineesh@gmail.com>
2026-03-23 16:29:55 +0530

    BottomBanner fix

--- App/client/src/components/BottomBanner.jsx
+++ App/client/src/components/BottomBanner.jsx
@@ -6,16 +6,29 @@ const BottomBanner = () => {
     <div className='relative mt-24'>
         <img src={assets.bottom_banner_image} alt="banner" className='w-full hidden md:block'/>
         <img src={assets.bottom_banner_image_sm} alt="banner" className='w-full  md:hidden'/>
-          <div className='absolute inset-0 flex flex-col item-center md:items-end md:justify-center pt-16 md:pt-0 md:pr-24'>
-            <div className=''>
+          <div className='absolute inset-0 flex flex-col items-center md:items-end md:justify-center pt-16 md:pt-0 md:pr-24'>
+            <div className='ml-20'>
                 <h1 className="">Why We Are The Best?</h1>
-                {features.map((feature,index) => (
-                        <div key={index} className='flex items-center gap-4 mt-2'>
-                            <img src={feature.icon} alt={feature.title} className='md:w-11 w-9'/>
-                            <h3 className='text-lg md:text-xl font-semibold'>{feature.title}</h3>
-                            <p className='text-gray-500/70 text-xs md:text-sm'>{feature.description}</p>
+                    {features.map((feature,index) => (
+                        <div key={index} className='flex items-start gap-4 mt-4'>
+                            
+                            <img 
+                            src={feature.icon} 
+                            alt={feature.title} 
+                            className='md:w-11 w-9'
+                            />
+
+                            <div>
+                            <h3 className='text-lg md:text-xl font-semibold'>
+                                {feature.title}
+                            </h3>
+                            <p className='text-gray-500/70 text-xs md:text-sm'>
+                                {feature.description}
+                            </p>
+                            </div>
+
                         </div>
-                ))}
+                        ))}
             </div>
         </div>
     </div>
