e4dd03f658b5ef8847936564e9c51612f8006f24
Sipsa <sipsabineesh@gmail.com>
2026-03-27 13:45:42 +0530

    cleanup

--- App/client/src/pages/Home.jsx
+++ App/client/src/pages/Home.jsx
@@ -4,10 +4,7 @@ const MainBanner = lazy(() => import('../components/MainBanner'))
 const Services = lazy(() => import('../components/Services'))
 const Contact = lazy(() => import('../components/Contact'))
 const BottomBanner = lazy(() => import('../components/BottomBanner'))
-// import MainBanner from '../components/MainBanner'
-// import Contact from '../components/Contact'
-// import BottomBanner from '../components/BottomBanner'
-// import Services from '../components/Services'
+
 
 const Home = () => {
   return (
